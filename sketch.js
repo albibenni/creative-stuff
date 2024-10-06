2ff048c8294abd602a2f26d5fbc87f04b64fbf08
Alberto Benatti <albi.benni8@gmail.com>
2024-10-06 17:24:08 +0200

    fix

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
-const canvasSketch = require('canvas-sketch');
+import canvasSketch from "canvas-sketch/lib/canvas-sketch.js";
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [2048, 2048],
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
   };
 };
